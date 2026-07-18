de2b14f94175e8de7f1051263ef7ff9ebce1f1ec
BacTv <zenofuture@gmail.com>
2026-07-19 03:25:22 +0700

    feat: validate training data sources

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -6,11 +6,22 @@ from src.training.fingerprints import (
     sha256_file,
     stable_json_sha256,
 )
+from src.training.sources import SourceSpec, load_source_records
+from src.training.validation import (
+    ValidationFinding,
+    require_valid_source,
+    validate_records,
+)
 
 __all__ = [
     "CanonicalEntity",
     "CanonicalRecord",
+    "SourceSpec",
+    "ValidationFinding",
     "fingerprint_files",
+    "load_source_records",
+    "require_valid_source",
     "sha256_file",
     "stable_json_sha256",
+    "validate_records",
 ]
