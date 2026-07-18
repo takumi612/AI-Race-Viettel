9692fce9366034b63063c5a20eddca35c2354f1a
BacTv <zenofuture@gmail.com>
2026-07-19 03:30:05 +0700

    feat: project task training datasets

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -7,6 +7,11 @@ from src.training.fingerprints import (
     stable_json_sha256,
 )
 from src.training.sources import SourceSpec, load_source_records
+from src.training.projections import (
+    project_embedding_seeds,
+    project_ner_records,
+    project_reranker_seeds,
+)
 from src.training.splits import (
     SplitAssignment,
     assert_no_split_leakage,
@@ -30,6 +35,9 @@ __all__ = [
     "build_trusted_folds",
     "fingerprint_files",
     "load_source_records",
+    "project_embedding_seeds",
+    "project_ner_records",
+    "project_reranker_seeds",
     "require_valid_source",
     "sha256_file",
     "stable_json_sha256",
